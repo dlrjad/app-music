4cd6526e2fbd39a5119cba30c35ac68809f1c037
Jorge <jorge@jorge.com>
2018-06-11 15:47:16 +0100

    artist in API

--- api/app.js
+++ api/app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var user_routes = require('./routes/user');
+var artist_routes = require('./routes/artist');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api', user_routes);
+app.use('/api', artist_routes);
 
 app.get('/pruebas', function(req, res){
     res.status(200).send({message: 'Mensaje de prueba'});
